cbb186d06409cea75f81ad8b7751defba1614cae
Adam Olson <adamlsn@gmail.com>
2021-06-03 21:08:50 -0500

    intial attempt at routes/api/breweryDB.js route

---
+++ routes/api/breweryDB.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const fetch = require("node-fetch");
+
+const city = "milwaukee";
+const url = `https://api.openbrewerydb.org/breweries?per_page=50&by_state=wisconsin&by_city=${city}`;
+
+fetch(url).then(
+    function(response){
+        if(response.status !== 200){
+            console.log(`Looks like there was a problem. Status Code: ${response.status}`)
+            return;
+        }
+
+        response.json().then(function(data){
+            const idArr = getID(data);
+            console.log(idArr);
+        })
+    }
+);
+
+getID = (data) => {
+    const arrLength = data.length;
+    const newData = []
+    for(i = 0; i < arrLength; i++) {
+        newData.push(data[i].id)
+    };
+    return newData;
+};
+
+module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,6 @@
 const router = require("express").Router();
+const breweryDB = require("./breweryDB");
+
+router.use("/brewery", breweryDB);
 
 module.exports = router;
