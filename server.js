1019136df5e9110ebd9cf5643da92f21ca0f811c
Your Name <you@example.com>
2021-06-05 12:49:01 -0500

    basic page structure through pug

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 // Dependencies
 // ==============================
-const express = require('express');
-
+const express = require("express");
+const pug = require("pug");
 
 //Sets up the Express App
 //===============================
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.get("/pug", (req, res) => {
+  res.send("pug");
+});
+
+app.get("/", (req, res) => {
+  var html = pug.renderFile("./pages/index.pug", {
+    youAreUsingPug: true,
+    pageTitle: "Best Page",
+  });
+
+  res.send(html);
+});
+
+app.get("/results", (req, res) => {
+  var html = pug.renderFile("./pages/results.pug", {
+    youAreUsingPug: true,
+    pageTitle: "Results Page",
+  });
+
+  res.send(html);
+});
 
 app.listen(PORT, () => {
-    console.log('Server listening on: '+PORT);
+  console.log("Server listening on: " + PORT);
 });
