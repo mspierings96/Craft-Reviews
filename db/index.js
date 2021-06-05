2f15c03a4a312fc45a06876e54d696d51d3d9d55
Devon <devonross50@yahoo.com>
2021-06-05 14:06:08 -0500

    revised TotalsByScore query to pull a ranking of highest score by greatest review count

--- db/index.js
+++ db/index.js
@@ -10,7 +10,63 @@ class DB {
 // find count of reviews by rating scores for brewery
 findTotalsByScore() {
     return this.connection.promise().query(
-        'Select r.apiID, COUNT(CASE WHEN r.review = 5 then 1 else NULL END) as "5-Star", COUNT(CASE WHEN r.review = 4 then 1 else NULL END) as "4-Star", COUNT(CASE WHEN r.review = 3 then 1 else NULL END) as "3-Star", COUNT(CASE WHEN r.review = 2 then 1 else NULL END) as "2-Star", COUNT(CASE WHEN r.review = 1 then 1 else NULL END) as "1-Star" From reviews r WHERE r.apiID=? Group By r.apiID;'
+        `Select a.apiID, a.Ranking, sum(a.ScoreCount) as ScoreCounts 
+        from (
+        Select apiID, '5Star' Ranking, 5Star ScoreCount
+        from (
+        Select r.apiID,
+        (Case When r.review = 5 then 1 else null end) as 5Star,
+        (Case When r.review = 4 then 2 else null end) as 4Star,
+        (Case When r.review = 3 then 3 else null end) as 3Star,
+        (Case When r.review = 2 then 4 else null end) as 2Star,
+        (Case When r.review = 1 then 5 else null end) as 1Star
+        from reviews r) as u
+        union all
+        select apiID, '4Star' Ranking, 4Star ScoreCount
+        from (
+        Select r.apiID,
+        (Case When r.review = 5 then 1 else null end) as 5Star,
+        (Case When r.review = 4 then 2 else null end) as 4Star,
+        (Case When r.review = 3 then 3 else null end) as 3Star,
+        (Case When r.review = 2 then 4 else null end) as 2Star,
+        (Case When r.review = 1 then 5 else null end) as 1Star
+        from reviews r) as u
+        union all
+        select apiID, '3Star' Ranking, 3Star ScoreCount
+        from (
+        Select r.apiID,
+        (Case When r.review = 5 then 1 else null end) as 5Star,
+        (Case When r.review = 4 then 2 else null end) as 4Star,
+        (Case When r.review = 3 then 3 else null end) as 3Star,
+        (Case When r.review = 2 then 4 else null end) as 2Star,
+        (Case When r.review = 1 then 5 else null end) as 1Star
+        from reviews r) as u
+        union all
+        select apiID, '2Star' Ranking, 2Star ScoreCount
+        from (
+        Select r.apiID,
+        (Case When r.review = 5 then 1 else null end) as 5Star,
+        (Case When r.review = 4 then 2 else null end) as 4Star,
+        (Case When r.review = 3 then 3 else null end) as 3Star,
+        (Case When r.review = 2 then 4 else null end) as 2Star,
+        (Case When r.review = 1 then 5 else null end) as 1Star
+        from reviews r) as u
+        union all
+        select apiID, '1Star' Ranking, 1Star ScoreCount
+        from (
+        Select r.apiID,
+        (Case When r.review = 5 then 1 else null end) as 5Star,
+        (Case When r.review = 4 then 2 else null end) as 4Star,
+        (Case When r.review = 3 then 3 else null end) as 3Star,
+        (Case When r.review = 2 then 4 else null end) as 2Star,
+        (Case When r.review = 1 then 5 else null end) as 1Star
+        from reviews r) as u
+        order by Ranking desc
+        -- limit 5
+        ) as a
+        where a.ScoreCount is not null
+        group by a.apiID
+        ;`
     );
 }
 
