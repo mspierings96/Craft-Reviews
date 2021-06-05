5a20fbd1f4ae658eda859dafd76e0ef0a1fdf45c
Devon <devonross50@yahoo.com>
2021-06-05 14:31:07 -0500

    reordered sql queries

--- db/index.js
+++ db/index.js
@@ -10,6 +10,20 @@ class DB {
 // find count of reviews by rating scores for brewery
 findTotalsByScore() {
     return this.connection.promise().query(
+        `Select r.apiID, COUNT(CASE WHEN r.review = 5 then 1 else NULL END) as "5-Star", 
+        COUNT(CASE WHEN r.review = 4 then 1 else NULL END) as "4-Star", 
+        COUNT(CASE WHEN r.review = 3 then 1 else NULL END) as "3-Star", 
+        COUNT(CASE WHEN r.review = 2 then 1 else NULL END) as "2-Star", 
+        COUNT(CASE WHEN r.review = 1 then 1 else NULL END) as "1-Star" 
+        From reviews r
+        WHERE r.apiID=?
+        Group by r.apiID;`
+    );
+}
+
+// find top 5 by highest rating for top 5 of homepage
+findHighestFive() {
+    return this.connection.promise().query(
         `Select a.apiID, a.Ranking, sum(a.ScoreCount) as ScoreCounts 
         from (
         Select apiID, '5Star' Ranking, 5Star ScoreCount
@@ -67,13 +81,6 @@ findTotalsByScore() {
         group by a.apiID
         limit 5
         ;`
-    );
-}
-
-// find top 5 by highest rating for top 5 of homepage
-findHighestFive() {
-    return this.connection.promise().query(
-        'select  r.apiID, COUNT(CASE WHEN r.review = 5 then 1 else NULL END) as 5Star from reviews r group by r.apiID order by 5Star desc LIMIT 5;'
     );
 }
 
