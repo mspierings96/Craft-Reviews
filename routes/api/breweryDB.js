eee7d7d524a09695e74bdbce9ccf8feec1f26ff9
Adam Olson <adamlsn@gmail.com>
2021-06-05 15:25:50 -0500

    tweaking in routes/html-routes.js

--- routes/api/breweryDB.js
+++ routes/api/breweryDB.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const fetch = require("node-fetch");
+// const inputValue = require("../../assets/script");
 
 const city = "madison";
 const url = `https://api.openbrewerydb.org/breweries?per_page=50&by_state=wisconsin&by_city=${city}`;
@@ -26,5 +27,26 @@ getID = (data) => {
     };
     return newData;
 };
+
+fetch("https://api.openbrewerydb.org/breweries/10707").then(
+    function(response){
+        if(response.status !== 200){
+            console.log(`Looks like there was a problem. Status Code: ${response.status}`)
+            return;
+        }
+
+        response.json().then(function(data){
+            console.log(`
+            Name: ${data.name}
+            City: ${data.city}
+            Website: ${data.website_url}
+            `)
+        })
+    }
+)
+
+// router.get("/:id", (req, res) => {
+    
+// })
 
 module.exports = router;
