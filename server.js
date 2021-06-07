5e92a123a226a9f759ba7ade467dd7428a490e69
nazliozemrek <77180888+nazliozemrek@users.noreply.github.com>
2021-06-07 18:53:42 -0500

    merge issues

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const sequelize = require("./config/connection-sequelize");
 const mysql = require("mysql2");
 
 const pug = require("pug");
+require('dotenv').config();
+
+const {auth, requiresAuth} =require('express-openid-connect');
 
 //Sets up the Express App
 //===============================
@@ -15,6 +18,33 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"));
+app.use(
+  auth({
+    authRequired:false,
+    auth0Logout:true,
+      issuerBaseURL:process.env.AUTH0_ISSUER_BASE_URL,
+      baseURL:process.env.BASE_URL,
+      clientID:process.env.AUTH0_CLIENT_ID,
+      secret:process.env.CLIENT_SECRET,
+
+  })
+);
+app.get('/login',(req,res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
+app.get('/sign-up',(req,res) => {
+  res.oidc.login({
+    authorizationParams:{
+      screen_hint:'sign-up',
+    },
+  });
+});
+app.use((req,res,next) => {
+  res.locals.isAuthenticated = req.oidc.isAuthenticated();
+  next();
+});
 
 app.get("/pug", (req, res) => {
   res.send("pug");
@@ -34,9 +64,17 @@ app.get("/results", (req, res) => {
     youAreUsingPug: true,
     pageTitle: "Results Page",
   });
-
   res.send(html);
 });
+app.get('/Profile',requiresAuth(),(req,res) => {
+  res.send(JSON.stringify(req.oidc.user))
+  var html = pug.renderFile("./pages/profile.pug",{
+    youAreUsingPug:true,
+    pageTitle:"Profile"
+  })
+  res.send(html);
+});
+
 
 app.listen(PORT, () => {
   console.log("Server listening on: " + PORT);
