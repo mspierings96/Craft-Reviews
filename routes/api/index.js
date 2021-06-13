4811d1f47fdb42aa0053d5c861abaa89817011b2
Adam Olson <adamlsn@gmail.com>
2021-06-13 18:51:11 -0500

    changing server.js to output to pug correctly

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,8 @@
 // const express = require("express");
 const router = require("express").Router();
-const breweryDB = require("./breweryDB");
 const rateRoutes = require("./rate-routes");
 const userRoutes = require("./user-routes");
 
-router.use("/brewery", breweryDB);
 router.use("/rate", rateRoutes);
 router.use("/user", userRoutes);
 
