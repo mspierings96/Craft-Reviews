44c3d5aebea716d81da55a34854611e4f7c7ce4a
Your Name <you@example.com>
2021-06-13 21:24:02 -0500

    changes to index.js in the db folder

--- db/index.js
+++ db/index.js
@@ -1,46 +1,37 @@
-const connection = require('../config/connection-mysql')
+const connection = require("../config/connection-sequelize");
 
 class DB {
-    // Keeping a reference to the connection on the class in case we need it later
-    constructor(connection) {
-      this.connection = connection;
-    }
+  // Keeping a reference to the connection on the class in case we need it later
+  constructor(connection) {
+    this.connection = connection;
+  }
 
-
-// find count of reviews by rating scores for brewery
-findTotalsByScore(apiID) {
-    return (
-        `SELECT r.apiID, AVG(r.review) AvgReview, count(r.review) ReviewCount
+  // find count of reviews by rating scores for brewery
+  findTotalsByScore(apiID) {
+    return `SELECT r.apiID, AVG(r.review) AvgReview, count(r.review) ReviewCount
         from reviews r
         Where r.apiID = ?
-        group by r.apiID;`
-    );
-}
+        group by r.apiID;`;
+  }
 
-// find top 5 by highest rating for top 5 of homepage
-findHighestFive() {
-    return (
-        `SELECT r.apiID, AVG(r.review) AvgReview, count(r.review) ReviewCount
+  // find top 5 by highest rating for top 5 of homepage
+  findHighestFive() {
+    return `SELECT r.apiID, AVG(r.review) AvgReview, count(r.review) ReviewCount
         from reviews r
         group by r.apiID
         order by AvgReview desc, ReviewCount desc
-        limit 5;`
-    );
-}
+        limit 5;`;
+  }
 
-// Search for existing review
-searchExistingReview(){
-    return (
-    `Select r.apiID, r.userName, r.review FROM reviews r
-    where r.apiID = ? and r.userName=?;`
-    );
-}
+  // Search for existing review
+  searchExistingReview() {
+    return `Select r.apiID, r.userName, r.review FROM reviews r
+    where r.apiID = ? and r.userName=?;`;
+  }
 
-// Search for existing username
-searchUserName(){
-    return (
-        'select u.userName from users u where u.userName = ?;'
-    );
-}
+  // Search for existing username
+  searchUserName() {
+    return "select u.userName from users u where u.userName = ?;";
+  }
 }
 module.exports = new DB(connection);
