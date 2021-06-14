6962e05bd616a1c1907a2a26404d77f4be29b1d7
Devon <devonross50@yahoo.com>
2021-06-14 17:45:55 -0500

    update host source for heroku

--- config/connection-mysql.js
+++ config/connection-mysql.js
@@ -5,7 +5,7 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection(
   process.env.JAWSDB_URL || {
-    host: "localhost",
+    host: "qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: process.env.DB_USER,
     password: process.env.DB_PW,
     database: process.env.DB_NAME,
