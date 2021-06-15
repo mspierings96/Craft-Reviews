c4374022db6d160be135650ff069c6fdc97e145b
Adam Olson <adamlsn@gmail.com>
2021-06-14 20:21:26 -0500

    removing console logs

--- public/rating.js
+++ public/rating.js
@@ -1,13 +1,10 @@
 $("a.dropdown-item").on("click", function (event) {
-  console.log($(this).text());
-  console.log($(this).data("apiid"));
   axios
     .post("/api/rate/setRating", {
       apiID: $(this).data("apiid"),
       review: $(this).text(),
     })
     .then(function (response) {
-      console.log(response);
       location.reload();
     });
 });
